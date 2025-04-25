3c1b5b71e3d8720f19cc220f1b79b8b96235ecdd
butuz0 <y.o.oryshchenko@gmail.com>
2025-04-25 17:19:41 +0300

    setup project settings

---
+++ api/config/settings/local.py
@@ -0,0 +1,26 @@
+from .base import *  # noqa
+from .base import BASE_DIR
+from os import path, getenv
+from dotenv import load_dotenv
+
+
+local_env_file = path.join(BASE_DIR, '.envs', '.env.local')
+
+if path.isfile(local_env_file):
+    load_dotenv(local_env_file)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+SITE_NAME = getenv('SITE_NAME')
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+ADMIN_URL = getenv('DJANGO_ADMIN_URL')
