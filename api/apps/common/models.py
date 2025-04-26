ff9e664a96d349f16ecd6569ff6fd0ff65e7150f
butuz0 <y.o.oryshchenko@gmail.com>
2025-04-26 16:04:19 +0300

    create abstract timestamped model and custom json renderer

--- api/apps/common/models.py
+++ api/apps/common/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+import uuid
 
-# Create your models here.
+User = get_user_model()
+
+
+class TimeStampedModel(models.Model):
+    '''
+    Abstract model which holds common fields for created models.
+    '''
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(verbose_name=_('Created At'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('Updated At'), auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', '-updated_at']
---
+++ api/apps/common/renderers.py
@@ -0,0 +1,33 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.renderers import JSONRenderer as BaseJSONRenderer
+from typing import Any, Optional
+import json
+
+
+class JSONRenderer(BaseJSONRenderer):
+    '''
+    Modifies the structure of rendered JSON response by
+    adding status_code and wrapping data in object_label key.
+    '''
+    charset = 'utf-8'
+    object_label = 'object'
+
+    def render(self, data: Any, accepted_media_type: Optional[str], renderer_context: Optional[dict]) -> bytes:
+        context = renderer_context or {}
+
+        view = context.get('view')
+        
+        object_label = getattr(view, 'object_label', self.object_label)
+
+        response = context.get('response')
+
+        if not response:
+            raise ValueError(_('Response not found in renderer context.'))
+
+        status_code = response.status_code
+        errors = data.get('errors', None)
+
+        if errors is not None:
+            return super(JSONRenderer, self).render(data, accepted_media_type, context)
+
+        return json.dumps({'status_code': status_code, object_label: data}).encode(self.charset)
