72ed317968d75050700828594920d118b055a642
butuz0 <y.o.oryshchenko@gmail.com>
2025-05-01 22:26:40 +0300

    delete celery invitation email task, move base.html to templates root

--- api/apps/teams/signals.py
+++ api/apps/teams/signals.py
@@ -1,10 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Invitation
-from .tasks import send_team_invitation_email_task
+from .emails import send_team_invitation_email
 
 
 @receiver(post_save, sender=Invitation)
 def create_celery_invitation_task(sender, instance: Invitation, created: bool, **kwargs):
     if created:
-        send_team_invitation_email_task.delay(instance.id)
+        user = instance.sent_to
+        send_team_invitation_email(user, instance)
