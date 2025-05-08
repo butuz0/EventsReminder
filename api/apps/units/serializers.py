a9d9b80e299a3dce2213df1bde160026b39b9217
butuz0 <y.o.oryshchenko@gmail.com>
2025-05-08 18:05:52 +0300

    return number of employees of faculties and departments

--- api/apps/units/serializers.py
+++ api/apps/units/serializers.py
@@ -1,14 +1,27 @@
 from .models import Faculty, Department
+from apps.profiles.models import Profile
 from rest_framework import serializers
 
 
 class FacultySerializer(serializers.ModelSerializer):
+    num_employees = serializers.SerializerMethodField()
+
     class Meta:
         model = Faculty
-        fields = ['id', 'faculty_name', 'faculty_abbreviation']
-  
-  
+        fields = ['id', 'faculty_name', 'faculty_abbreviation',
+                  'num_employees']
+
+    def get_num_employees(self, obj: Faculty) -> int:
+        return Profile.objects.filter(department__faculty=obj).count()
+
+
 class DepartmentsSerializer(serializers.ModelSerializer):
+    num_employees = serializers.SerializerMethodField()
+
     class Meta:
         model = Department
-        fields = ['id', 'department_name', 'department_abbreviation', 'faculty']
+        fields = ['id', 'department_name', 'department_abbreviation',
+                  'faculty', 'num_employees']
+
+    def get_num_employees(self, obj: Department) -> int:
+        return Profile.objects.filter(department=obj).count()
