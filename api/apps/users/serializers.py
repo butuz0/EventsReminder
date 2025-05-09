f80148f8e4a428fc36da26015099c65a69cdaa52
butuz0 <y.o.oryshchenko@gmail.com>
2025-05-09 13:45:32 +0300

    fix telegram fields in custom user serializer

--- api/apps/users/serializers.py
+++ api/apps/users/serializers.py
@@ -16,15 +16,15 @@ class CustomUserSerializer(UserSerializer):
     gender = serializers.SerializerMethodField()
     position = serializers.ReadOnlyField(source='profile.position')
     phone_number = PhoneNumberField(source='profile.phone_number')
-    telegram_username = serializers.ReadOnlyField(source='profile.telegram_username')
-    telegram_phone_number = PhoneNumberField(source='profile.telegram_phone_number')
-    
+    telegram_username = serializers.ReadOnlyField(source='profile.telegram.telegram_username')
+    telegram_phone_number = PhoneNumberField(source='profile.telegram.telegram_phone_number')
+
     def get_gender(self, obj) -> str | None:
         return obj.profile.get_gender_display()
     
     class Meta(UserSerializer.Meta):
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 
-                  'full_name', 'gender', 'position', 'phone_number', 
+        fields = ['id', 'email', 'first_name', 'last_name',
+                  'full_name', 'gender', 'position', 'phone_number',
                   'telegram_username', 'telegram_phone_number']
         read_only_fields = ['id', 'email']
