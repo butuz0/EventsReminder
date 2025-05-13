768abd0e7a76a6b1cd3462a33aa9355d7797e657
butuz0 <y.o.oryshchenko@gmail.com>
2025-05-14 00:06:33 +0300

    save current user information in redux store

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -1,13 +1,17 @@
 import FormHeader from "@/components/forms/FormHeader";
 import LoginForm from "@/components/forms/auth/LoginForm";
+import {Metadata} from "next";
 
+export const metadata: Metadata = {
+  title: "KPI Notify | Log In"
+}
 
 export default function LoginPage() {
   return (
     <div className="w-full">
       <FormHeader
         title="Вхід в акаунт"
-        staticText="Ще не маєте акаунта?"
+        staticText="Ще не маєте акаунту?"
         linkText="Реєстрація"
         linkHref={"/register"}
       />
